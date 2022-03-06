6b16f80ac4001095f24bff817e0f043e9ee0c0f8
kitkatmia <alinka.markova@gmail.com>
2022-03-06 11:42:43 -0800

    Updated file name

--- src/tools/Firebase.jsx
+++ src/tools/Firebase.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "./Secrets";
+import firebaseConfig from "./secrets.jsx";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
