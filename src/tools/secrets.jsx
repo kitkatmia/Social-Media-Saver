df05afcc8d6bb5618d2bac745ed1f6b71a8bf3d7
kitkatmia <alinka.markova@gmail.com>
2022-03-12 19:33:40 -0800

    Removed from gitignore

---
+++ src/tools/secrets.jsx
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeYx-QbGq79RNxSQhkqXo6yErkO11IsCY",
+    authDomain: "social-media-saver-58dfd.firebaseapp.com",
+    projectId: "social-media-saver-58dfd",
+    storageBucket: "social-media-saver-58dfd.appspot.com",
+    messagingSenderId: "567651085551",
+    appId: "1:567651085551:web:87fd52b3b155665da7ff1a",
+    measurementId: "G-KS1ZCQ95QL"
+};
+
+export default firebaseConfig
+
+// This code isn't pushed to the internet because it's in the gitignore file. This is how you keep your database passwords a secret. You can import the information in other files.
